5fd79a9d23ead9f00f5f93be4a94cf2752272d89
Maciej Lenartowicz <lenarmathias@gmail.com>
2024-03-22 14:41:44 +0100

    Initial setup of App style including theme, GlobalStyle and Normalize

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { GlobalStyle } from './styled';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Normalize />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+    colors: {
+        backgroundOne: "#2A9DF4", // butterflyBlue Color
+        backgroundTwo: "#187BCD", // waterBlue Color
+    }
+};
